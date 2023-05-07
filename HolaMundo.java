829c9fe4fcedf992d08a1b5c037a0c3786c3c09c
unknown <kyare@unsa.edu.pe>
2023-05-07 09:43:40 -0500

    Imprimir nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 public class HolaMundo{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("¡Hola Mundo!");
         System.out.println("Bienvenida al curso");
+        System.out.println("¿Cuál es tu nombre?");
+        System.out.println("¡Hola " + sc.nextLine() + "!");
     }
 }

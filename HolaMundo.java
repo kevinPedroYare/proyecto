2b16a3032c7894c0bc7094e72f2cb1f73e5b0880
unknown <kyare@unsa.edu.pe>
2023-05-07 09:41:58 -0500

    Bienvenida al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class HolaMundo{
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
+        System.out.println("Bienvenida al curso");
     }
 }
